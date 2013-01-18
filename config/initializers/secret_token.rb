b31c1a7f4b2da7f586cdee9c458431e0dceb0786
Peer Allan <pallan@allanweb.ca>
2013-01-18 07:54:30 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+McaDraws::Application.config.secret_token = '5049454fff1c10d0b62e452c5b65154b3f7296b375d1b9d4009a8b57129034178821488baaf5d104688c069f277281b946f5b3cee43180edba552b43345d4d8e'
