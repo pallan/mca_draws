43546d75b778b8ea11f73298218138dcf4e10df0
Peer Allan <peer.allan@canadadrugs.com>
2013-01-24 06:57:16 -0600

    Allow brackets to be calculated and drawn using raphael

--- app/controllers/bonspiels_controller.rb
+++ app/controllers/bonspiels_controller.rb
@@ -3,9 +3,24 @@ class BonspielsController < ApplicationController
   caches_page :show
   caches_page :all
   
-  def index
-    b = Bonspiel.find(1)
-    redirect_to bonspiel_path(b)
+  def index    
+    rinks = Rink.limit(110).order('RAND()').all.map(&:name)    
+    
+
+    # pad the rinks to a power of 2
+    bracket_size = next_power_of_two(rinks.size)
+    rinks += Array.new(bracket_size-rinks.size)
+    pairings = seed_order(bracket_size).map { |index| rinks[index] }.each_slice(2).to_a
+
+    # Simulates the matches being played
+    round_count = (Math.log(bracket_size) / Math.log(2)).to_i
+    rounds = Array.new(round_count,[])
+    rounds[0] = pairings
+
+    0.upto(round_count-1) do |r|
+      rounds[r+1] = rounds[r].map {|pair| pair.compact.shuffle.first }.each_slice(2).to_a
+    end
+    @rounds = JSON.generate({:rounds => rounds})
   end
   
   def show
@@ -65,4 +80,39 @@ class BonspielsController < ApplicationController
     
     render :text => "Load attempted: #{draw_links.map{|l| l['href']}.inspect}"
   end
+
+  def next_power_of_two(n)
+    return n if (n-1)&n == 0
+    pow=1
+    while (n >= 0x100000000) do  pow += 32; n >>= 32; end
+    if (n & 0xFFFF0000 > 0) then pow += 16; n >>= 16; end
+    if (n & 0xFF00 > 0)     then pow += 8;  n >>= 8;  end
+    if (n & 0xF0 > 0)       then pow += 4;  n >>= 4; end
+    if (n & 0xC > 0)        then pow += 2;  n >>= 2; end
+    if (n & 0x2 > 0)        then pow += 1;  n >>= 1; end
+    1<<pow
+  end
+  
+  def seed_order(size)
+    bracket_list = []
+    0.upto(size-1) do |i|
+      bracket_list << i
+    end
+
+    slice = 1
+    while slice < bracket_list.length/2
+      temp = bracket_list
+      bracket_list = []
+
+      while temp.length > 0
+        bracket_list.concat temp.slice!(0, slice)       # n from the beginning
+        bracket_list.concat temp.slice!(-slice, slice)  # n from the end
+      end
+
+      slice *= 2
+    end
+
+    bracket_list
+  end  
+
 end
